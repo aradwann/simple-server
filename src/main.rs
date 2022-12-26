0e2241f999d1fe77f4ad94ba896dffd53de0e4a1
Ahmed Radwan <aradwann@proton.me>
2022-12-26 23:07:46 +0200

    serve html through single threaded server

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
-use std::net::TcpListener;
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    let listener = TcpListener::bind("localhost:7878").unwrap();
+    // this listener is a tcp listener binded by the determined port
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    // a for loop on connections (connection trials) received on the listens
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        println!("Connection established");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // buffer_reader is a buffer made by taking in the incoming stream
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
